db79c6175939fed44bb19c21e78ff97a2d560de2
Borzen <Borzen00@hotmail.com>
2025-10-25 10:33:23 -0500

    Starting over from scratch

--- Content/DataAccess/JACMS.Content.APIClient/Models/Requests/TextContentCreateRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JACMS.Content.APIClient.Models.Requests
-{
-    public class TextContentCreateRequest
-    {
-        public string TextValue { get; set; }
-        public int TextTypeId { get; set; }
-    }
-}
--- Content/DataAccess/JACMS.Content.APIClient/Models/Requests/TextTypeRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JACMS.Content.APIClient.Models.Requests
-{
-    public class TextTypeRequest
-    {
-        public string TypeName { get; set; }
-        public string TypeDescription { get; set; }
-        public string Style { get; set; }
-    }
-}
