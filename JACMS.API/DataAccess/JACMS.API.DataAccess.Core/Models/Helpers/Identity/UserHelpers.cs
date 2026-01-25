6ba663e04082833789b803ee0970b04aef0d3dd0
Borzen <Borzen00@hotmail.com>
2026-01-25 13:23:59 -0600

    Fixed a problem that I caused

--- JACMS.API/DataAccess/JACMS.API.DataAccess.Core/Models/Helpers/Identity/UserHelpers.cs
+++ JACMS.API/DataAccess/JACMS.API.DataAccess.Core/Models/Helpers/Identity/UserHelpers.cs
@@ -12,7 +12,7 @@ namespace JACMS.API.DataAccess.Core.Models.Helpers.Identity
 {
     public static class UserHelpers
     {
-        public static DynamicParameters GetCreateDynamicParams(this User user, char seperator = '@')
+        public static DynamicParameters GetCreateDynamicParams(this User user, char seperator = '@', bool useSnakeCase = false)
         {
             if(user == null)
             {
@@ -21,20 +21,20 @@ namespace JACMS.API.DataAccess.Core.Models.Helpers.Identity
 
             DynamicParameters dynamicParams = new DynamicParameters();
 
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"UserName",seperator), user.UserName);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"NormalizedUserName",seperator), user.NormalizedUserName);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"Email",seperator), user.Email);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"NormalizedEmail",seperator), user.NormalizedEmail);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"EmailConfirmed",seperator), user.EmailConfirmed);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"PasswordHash",seperator), user.PasswordHash);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"SecurityStamp",seperator), user.SecurityStamp, System.Data.DbType.String);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"ConcurrencyStamp",seperator), user.ConcurrencyStamp, System.Data.DbType.String);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"PhoneNumber",seperator), user.PhoneNumber, System.Data.DbType.String);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"PhoneNumberConfirmed",seperator), user.PhoneNumberConfirmed);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"TwoFactorEnabled",seperator), user.TwoFactorEnabled);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"LockoutEnd",seperator), user.LockoutEnd, System.Data.DbType.DateTimeOffset);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"LockoutEnabled",seperator), user.LockoutEnabled);
-            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"AccessFailedCount",seperator), user.AccessFailedCount);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"UserName",seperator,useSnakeCase), user.UserName);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"NormalizedUserName",seperator, useSnakeCase), user.NormalizedUserName);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"Email",seperator, useSnakeCase), user.Email);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"NormalizedEmail",seperator, useSnakeCase), user.NormalizedEmail);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"EmailConfirmed",seperator, useSnakeCase), user.EmailConfirmed);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"PasswordHash",seperator, useSnakeCase), user.PasswordHash);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"SecurityStamp",seperator, useSnakeCase), user.SecurityStamp, System.Data.DbType.String);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"ConcurrencyStamp",seperator, useSnakeCase), user.ConcurrencyStamp, System.Data.DbType.String);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"PhoneNumber",seperator, useSnakeCase), user.PhoneNumber, System.Data.DbType.String);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"PhoneNumberConfirmed",seperator, useSnakeCase), user.PhoneNumberConfirmed);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"TwoFactorEnabled",seperator, useSnakeCase), user.TwoFactorEnabled);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"LockoutEnd",seperator, useSnakeCase), user.LockoutEnd, System.Data.DbType.DateTimeOffset);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"LockoutEnabled",seperator, useSnakeCase), user.LockoutEnabled);
+            dynamicParams.Add(ParamaterFormatterHelper.FormatPram($"AccessFailedCount",seperator, useSnakeCase), user.AccessFailedCount);
 
             return dynamicParams;
         }
--- JACMS.API/DataAccess/JACMS.API.DataAccess.PostgreSQL/Repositories/Identity/User/UserRepository.cs
+++ JACMS.API/DataAccess/JACMS.API.DataAccess.PostgreSQL/Repositories/Identity/User/UserRepository.cs
@@ -57,7 +57,7 @@ namespace JACMS.API.DataAccess.PostgreSQL.Repositories.Identity
             {
                 try
                 {
-                    var dynamicParams = user.GetCreateDynamicParams(':');
+                    var dynamicParams = user.GetCreateDynamicParams(':', true);
                     dynamicParams.Add(":new_user_id", direction: System.Data.ParameterDirection.Output);
                     await connection.ExecuteAsync(SQLCommands.Identity.User.Create, dynamicParams, commandType: System.Data.CommandType.StoredProcedure);
                     var newUserId = dynamicParams.Get<long>("new_user_id");
